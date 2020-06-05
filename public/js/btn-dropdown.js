7bb5078c262175680741929d1df7c925eb7ccb6e
SoulKaze <marco_oliver10@outlook.com>
2020-06-05 13:15:55 -0300

    Atualizando dropdown

--- public/js/btn-dropdown.js
+++ public/js/btn-dropdown.js
@@ -5,12 +5,12 @@ $(document).ready(() => {
       $dropdown = $(e.currentTarget).parent().parent();
       $arrow = $(e.currentTarget).children();
   
-      if($($arrow).hasClass('fa-arrow-up')) {
-        $($arrow).removeClass();
-        $($arrow).addClass('fas fa-arrow-down');
-      } else {
+      if($($arrow).hasClass('fa-arrow-down')) {
         $($arrow).removeClass();
         $($arrow).addClass('fas fa-arrow-up');
+      } else {
+        $($arrow).removeClass();
+        $($arrow).addClass('fas fa-arrow-down');
       }
   
       $target = $($dropdown).children('.dropdown');
@@ -24,15 +24,22 @@ $(document).ready(() => {
   if(window.innerWidth > 768) {
     $('#arrow-right').removeClass();
     $('#arrow-right').addClass('fas fa-arrow-right');
+
+    $('.navbar__rel').on('mouseenter mouseleave', (e) => {
+  
+      $target = $(e.currentTarget).children('.dropdown');
+  
+      $($target).slideToggle();
+      
+    });
   }
 
-  let timeout;
-  $('.navbar__rel').on('mouseenter mouseleave', (e) => {
-
-    $target = $(e.currentTarget).children('.dropdown');
-
-    $($target).slideToggle();
-    
+  $(window).resize(() => {
+    if(window.innerWidth > 768) {
+      $('#arrow-right').removeClass();
+      $('#arrow-right').addClass('fas fa-arrow-right');
+    }
   })
+  
 
 })
